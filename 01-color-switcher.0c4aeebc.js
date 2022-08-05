c377556b7d823b7c112b76eb1848b9071038bfb3
Aleksandr-Alekseenko-23 <105487472+Aleksandr-Alekseenko-23@users.noreply.github.com>
2022-08-05 17:54:45 +0000

    Deploying to gh-pages from @ Aleksandr-Alekseenko-23/goit-js-hw-09@1f1b78bf2178fc83d553017216caa4d7fac65b87 🚀

---
+++ 01-color-switcher.0c4aeebc.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop");console.log(t,e)}();
+//# sourceMappingURL=01-color-switcher.0c4aeebc.js.map
---
+++ 01-color-switcher.54c68023.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("[data-start]"),e=document.querySelector("[data-stop");console.log(t,e);
+//# sourceMappingURL=01-color-switcher.54c68023.js.map
